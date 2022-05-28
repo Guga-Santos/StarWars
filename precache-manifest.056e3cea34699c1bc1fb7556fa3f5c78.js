8d29067d6ed21e63bbbae2f8d56bf4e0f060fb91
GugaSantos <guga.santos.guga@gmail.com>
2022-05-28 19:13:34 -0300

    Updates

---
+++ precache-manifest.056e3cea34699c1bc1fb7556fa3f5c78.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc3d5f628b694f2f942c237688c8037",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "023f3c0029183dd3ecc5",
+    "url": "/StarWars/static/css/main.15215bb1.chunk.css"
+  },
+  {
+    "revision": "2cde2a32eba5440c45d8",
+    "url": "/StarWars/static/js/2.6efc746b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/StarWars/static/js/2.6efc746b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023f3c0029183dd3ecc5",
+    "url": "/StarWars/static/js/main.e0ee0ffd.chunk.js"
+  },
+  {
+    "revision": "dac716934fea99b38689",
+    "url": "/StarWars/static/js/runtime-main.0e107cbd.js"
+  },
+  {
+    "revision": "2835f42e4ca9aed568bc5f51062b510b",
+    "url": "/StarWars/static/media/Alderaan.2835f42e.webp"
+  },
+  {
+    "revision": "b59bec0ecb9f1a19c61f7b7a792c5181",
+    "url": "/StarWars/static/media/Bespin.b59bec0e.webp"
+  },
+  {
+    "revision": "bcfb3bfce785a805a8501dc9614d93a9",
+    "url": "/StarWars/static/media/Coruscant.bcfb3bfc.webp"
+  },
+  {
+    "revision": "90c06e8a61d2649940f47ffa62061523",
+    "url": "/StarWars/static/media/Dagobah.90c06e8a.webp"
+  },
+  {
+    "revision": "7e844afdba03c3d16042c11bbf57aa43",
+    "url": "/StarWars/static/media/Endor.7e844afd.webp"
+  },
+  {
+    "revision": "eaea61b662015c281ba476d377c4e89f",
+    "url": "/StarWars/static/media/FUNDO.eaea61b6.webp"
+  },
+  {
+    "revision": "374ea5f4598dcb6523a0ce4142f9254e",
+    "url": "/StarWars/static/media/Hoth.374ea5f4.webp"
+  },
+  {
+    "revision": "73d2c388b93bcf9829d0300dd1df7f33",
+    "url": "/StarWars/static/media/Kamino.73d2c388.webp"
+  },
+  {
+    "revision": "1060cc6703a76b1a058abe507d79eb19",
+    "url": "/StarWars/static/media/Naboo.1060cc67.webp"
+  },
+  {
+    "revision": "af7c07faaff0d16720c25db7990579e2",
+    "url": "/StarWars/static/media/Starjedi.af7c07fa.ttf"
+  },
+  {
+    "revision": "f4942ec25f2b3f3587ca6c5c1c6d059e",
+    "url": "/StarWars/static/media/Tatooine.f4942ec2.webp"
+  },
+  {
+    "revision": "611e16961297a1b382dc7207f962296d",
+    "url": "/StarWars/static/media/Yavin4.611e1696.webp"
+  },
+  {
+    "revision": "ce5e1211f818738680d057768c7e091d",
+    "url": "/StarWars/static/media/starWars.ce5e1211.png"
+  }
+]);
--- src/context/PlanetsContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const PlanetsContext = createContext();
-
-export default PlanetsContext;
